ecfeb26a72e72d486740c45150ef0a6f861e4dfa
jeengbe <jesper.engberg@gmx.at>
2020-08-01 21:35:42 +0200

    [Fix] Anpassung an neue Anfragen

--- anfragen/elemente/meldung.php
+++ anfragen/elemente/meldung.php
@@ -1,26 +1,31 @@
 <?php
 Anfrage::post("art", "titel", "inhalt");
-Anfrage::post(false, "icon", "aktionen");
+Anfrage::post(false, "icon");
+
+if($icon === "undefined") {
+  $icon = null;
+}
+
 if($icon !== null) {
   $icon = new UI\Icon($icon);
 }
 
-$r = new UI\Meldung($titel, $inhalt, $art, $icon);
-
-$knoepfe = [];
-
-for ($i=0; $i<$aktionen; $i++) {
-  $knopfinhalt = "knopfinhalt$i";
-  $knopfziel = "knopfziel$i";
-  $knopfart = "knopfart$i";
-  Anfrage::post($knopfinhalt, $knopfziel);
-  Anfrage::post(false, $knopfart);
-  Anfrage::hatFehler();
-
-  $knopf = new UI\Knopf($$knopfinhalt, $$knopfart);
-  $knopf->addFunktion("onclick", $$knopfziel);
-  $knoepfe[] = $knopf;
-}
-
-echo Anfrage::antwort("Meldung", null, $r->__toString(), $knoepfe);
+Anfrage::setTyp("Code");
+Anfrage::setRueck("Code", new UI\Meldung($titel, $inhalt, $art, $icon));
+// $knoepfe = [];
+//
+// for ($i = 0; $i < $aktionen; $i++) {
+//   $knopfinhalt = "knopfinhalt$i";
+//   $knopfziel = "knopfziel$i";
+//   $knopfart = "knopfart$i";
+//   Anfrage::post($knopfinhalt, $knopfziel);
+//   Anfrage::post(false, $knopfart);
+//   Anfrage::checkFehler();
+//
+//   $knopf = new UI\Knopf($$knopfinhalt, $$knopfart);
+//   $knopf->addFunktion("onclick", $$knopfziel);
+//   $knoepfe[] = $knopf;
+// }
+//
+// echo Anfrage::antwort("Meldung", null, $r->__toString(), $knoepfe);
 ?>
--- js/meldungen.js
+++ js/meldungen.js
@@ -1,11 +1,15 @@
-ui.meldung = {
-  erfolg:           (t, b) => ui.meldung.meldung("Erfolg", t, b),
-  fehler:           (t, b) => ui.meldung.meldung("Fehler", t, b),
-  warnung:          (t, b) => ui.meldung.meldung("Warnung", t, b),
-  information:      (t, b) => ui.meldung.meldung("Information", t, b),
-  laden:            (t, b) => ui.meldung.meldung("Laden", t, b),
-  eingeschraenkt:   (t, b) => ui.meldung.meldung("Eingeschraenkt", t, b),
-  gesperrt:         (t, b) => ui.meldung.meldung("Gesperrt", t, b),
+ui.meldungen = {
+  erfolg:           (t, b) => ui.meldungen.meldung("Erfolg", t, b),
+  fehler:           (t, b) => ui.meldungen.meldung("Fehler", t, b),
+  warnung:          (t, b) => ui.meldungen.meldung("Warnung", t, b),
+  information:      (t, b) => ui.meldungen.meldung("Information", t, b),
+  laden:            (t, b) => ui.meldungen.meldung("Laden", t, b),
+  eingeschraenkt:   (t, b) => ui.meldungen.meldung("Eingeschraenkt", t, b),
+  gesperrt:         (t, b) => ui.meldungen.meldung("Gesperrt", t, b),
 
-  meldung: (art, titel, inhalt, icon) => core.ajax("UI", 1, null, {art: art, titel: titel, inhalt: inhalt, icon: icon})
+  meldung: (art, titel, inhalt, icon) => {
+    return new Promise((fertig) => {
+      core.ajax("UI", 1, null, {art: art, titel: titel, inhalt: inhalt, icon: icon}).then((r) => fertig(r.Code));
+    });
+  }
 };
