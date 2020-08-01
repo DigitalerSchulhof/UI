ea2ce2ce155c763fdf53afb621f3a8ec7367e5ff
Patrick Wagner <pawa@matinf.de>
2020-08-01 12:02:12 +0200

    [FIX] Anpassung Meldung auf neue Fenster und JSON

--- anfragen/elemente/meldung.php
+++ anfragen/elemente/meldung.php
@@ -1,11 +1,26 @@
 <?php
+Anfrage::post("art", "titel", "inhalt");
+Anfrage::post(false, "icon", "aktionen");
+if($icon !== null) {
+  $icon = new UI\Icon($icon);
+}
 
-  Anfrage::post("art", "titel", "inhalt");
-  Anfrage::post(false, "icon");
-  $icon = null;
-  if($icon !== "undefined") {
-    $icon = new UI\Icon($icon);
-  }
-  $r = new UI\Meldung($titel, $inhalt, $art, $icon);
-  echo $r;
+$r = new UI\Meldung($titel, $inhalt, $art, $icon);
+
+$knoepfe = [];
+
+for ($i=0; $i<$aktionen; $i++) {
+  $knopfinhalt = "knopfinhalt$i";
+  $knopfziel = "knopfziel$i";
+  $knopfart = "knopfart$i";
+  Anfrage::post($knopfinhalt, $knopfziel);
+  Anfrage::post(false, $knopfart);
+  Anfrage::hatFehler();
+
+  $knopf = new UI\Knopf($$knopfinhalt, $$knopfart);
+  $knopf->addFunktion("onclick", $$knopfziel);
+  $knoepfe[] = $knopf;
+}
+
+echo Anfrage::antwort("Meldung", null, $r->__toString(), $knoepfe);
 ?>
