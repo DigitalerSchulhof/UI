4b4cfeacbc92c27ece8527c528c8db6550a8c58c
jeengbe <jesper.engberg@gmx.at>
2020-08-13 10:53:18 +0200

    [Neu] brleft/-right bei Meldungen
    Das Attribut brleft/-right von Meldungen funktioniert als Aktion, 
    ähnlich wie "onclick" etc. und wird ausgelöst, wenn von einer Meldung 
    der linke bzw. rechte Rand angeklickt wird. Die Standardmäßige Aktion 
    (Gegenüberliegenden Rahmen ein-/ausfahren) wird dabei deaktiviert.
    @oxydon

--- js/ui.js
+++ js/ui.js
@@ -507,28 +507,36 @@ ui.meldung = {
     }
     let ts = getComputedStyle(t[0]);
     if(ev.offsetX < 0) {
-      let i2          = t.finde("i.i2.dshUiIcon");
-      let fehlercodes = t.finde(".dshFehlercode");
-      if(ts["border-right-width"] === "23px") {
-        t.setCss("border-right-width", "");
-        i2.setCss("right", "");
-        fehlercodes.setCss("right", "");
+      if(t.hatAttr("brleft")) {
+        new Function(t.getAttr("brleft")).apply(t);
       } else {
-        t.setCss("border-right-width", "23px");
-        let r = getComputedStyle(i2[0]).right;
-        i2.setCss("right", (-Math.abs(r.substr(0, r.length - 2))) + "px");
-        fehlercodes.setCss("right", "-23px");
+        let i2          = t.finde("i.i2.dshUiIcon");
+        let fehlercodes = t.finde(".dshFehlercode");
+        if(ts["border-right-width"] === "23px") {
+          t.setCss("border-right-width", "");
+          i2.setCss("right", "");
+          fehlercodes.setCss("right", "");
+        } else {
+          t.setCss("border-right-width", "23px");
+          let r = getComputedStyle(i2[0]).right;
+          i2.setCss("right", (-Math.abs(r.substr(0, r.length - 2))) + "px");
+          fehlercodes.setCss("right", "-23px");
+        }
       }
     }
     if(ev.offsetX > t[0].clientWidth) {
-      let i1 = t.finde("i.i1.dshUiIcon");
-      if(ts["border-left-width"] === "23px") {
-        t.setCss("border-left-width", "2px");
-        let l = getComputedStyle(i1[0]).left;
-        i1.setCss("left", Math.abs(l.substr(0, l.length - 2)) + "px");
+      if(t.hatAttr("brright")) {
+        new Function(t.getAttr("brright")).apply(t);
       } else {
-        t.setCss("border-left-width", "");
-        i1.setCss("left", "");
+        let i1 = t.finde("i.i1.dshUiIcon");
+        if(ts["border-left-width"] === "23px") {
+          t.setCss("border-left-width", "2px");
+          let l = getComputedStyle(i1[0]).left;
+          i1.setCss("left", Math.abs(l.substr(0, l.length - 2)) + "px");
+        } else {
+          t.setCss("border-left-width", "");
+          i1.setCss("left", "");
+        }
       }
     }
     if(t.ist(".dshUiMeldungLaden") && (ev.offsetY < 0 || ev.offsetY > t[0].clientHeight)) {
