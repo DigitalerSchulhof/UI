57f9d92b10dc3bb5ef25108b741a84780eb4865c
jeengbe <jesper.engberg@gmx.at>
2020-12-05 19:22:42 +0100

    [Fix] Fehlende Exports

--- ts/export.ts
+++ ts/export.ts
@@ -2,10 +2,28 @@ import * as laden from "./elemente/laden";
 import * as meldung from "./elemente/meldung";
 import * as togglegruppe from "./elemente/togglegruppe";
 import * as tabelle from "./elemente/tabelle";
+import * as formular from "./elemente/formular";
+import * as fenster from "./elemente/fenster";
+import * as datumsanzeige from "./elemente/datumsanzeige";
+import * as schieber from "./elemente/schieber";
+import * as toggle from "./elemente/toggle";
+import * as farbbeispiel from "./elemente/farbbeispiel";
+import * as passwort from "./elemente/passwort";
+import * as mail from "./elemente/mail";
+import * as reiter from "./elemente/reiter";
 
 export default {
   laden: laden,
   meldung: meldung,
   togglegruppe: togglegruppe,
-  tabelle: tabelle
+  tabelle: tabelle,
+  formular: formular,
+  fenster: fenster,
+  datumsanzeige: datumsanzeige,
+  schieber: schieber,
+  toggle: toggle,
+  farbbeispiel: farbbeispiel,
+  passwort: passwort,
+  mail: mail,
+  reiter: reiter,
 };
